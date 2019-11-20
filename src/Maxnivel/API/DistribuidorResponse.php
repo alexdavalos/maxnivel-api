23e4def12195195e8da43e3557c2cacaa36cba6d
Alexander <alex@latu.com.br>
2019-11-20 13:09:58 -0300

    adicionadas informacoes do distribuidor, e suas respeitivas funcoes getters e setters

--- src/Maxnivel/API/DistribuidorResponse.php
+++ src/Maxnivel/API/DistribuidorResponse.php
@@ -17,6 +17,20 @@ class DistribuidorResponse extends BaseResponse
     protected $cpf;
     protected $cnpj;
     protected $status;
+    protected $rg;
+    protected $usuario; //sobrenome do cliente no pedido
+    protected $nome;
+    protected $nit;
+    protected $ie;
+    protected $razao_social;
+    protected $nome_fantasia;
+    protected $cidade;
+    protected $bairro;
+    protected $endereco;
+    protected $complemento;
+    protected $numero;
+    protected $telefones;
+
 
     /**
      * @return mixed
@@ -98,4 +112,214 @@ class DistribuidorResponse extends BaseResponse
         $this->status = $status_distribuidor;
     }
 
+    /**
+     * @return mixed
+     */
+    public function getRgDistribuidor()
+    {
+        return $this->rg;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function setRgDistribuidor($rg_distribuidor)
+    {
+        $this->rg = $rg_distribuidor;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsuarioDistribuidor()
+    {
+        return $this->usuario;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function setUsuarioDistribuidor($usuario_distribuidor)
+    {
+        $this->usuario = $usuario_distribuidor;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNomeDistribuidor()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function setNomeDistribuidor($nome_distribuidor)
+    {
+        $this->nome = $nome_distribuidor;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNitDistribuidor()
+    {
+        return $this->nit;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function setNitDistribuidor($nit_distribuidor)
+    {
+        $this->nit = $nit_distribuidor;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIeDistribuidor()
+    {
+        return $this->ie;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function setIeDistribuidor($ie_distribuidor)
+    {
+        $this->ie = $ie_distribuidor;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRazaoSocialDistribuidor()
+    {
+        return $this->razao_social;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function setRazaoSocialDistribuidor($razao_social_distribuidor)
+    {
+        $this->razao_social = $razao_social_distribuidor;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNomeFantasiaDistribuidor()
+    {
+        return $this->nome_fantasia;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function setNomeFantasiaDistribuidor($nome_fantasia_distribuidor)
+    {
+        $this->nome_fantasia = $nome_fantasia_distribuidor;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getCidadeDistribuidor()
+    {
+        return $this->cidade;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function setCidadeDistribuidor($cidade_distribuidor)
+    {
+        $this->cidade = $cidade_distribuidor;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getBairroDistribuidor()
+    {
+        return $this->bairro;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function setBairroDistribuidor($bairro_distribuidor)
+    {
+        $this->bairro = $bairro_distribuidor;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEnderecoDistribuidor()
+    {
+        return $this->endereco;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function setEnderecoDistribuidor($endereco_distribuidor)
+    {
+        $this->endereco = $endereco_distribuidor;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getComplementoDistribuidor()
+    {
+        return $this->complemento;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function setCompletoDistribuidor($complemento_distribuidor)
+    {
+        $this->complemento = $complemento_distribuidor;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNumeroDistribuidor()
+    {
+        return $this->numero;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function setNumeroDistribuidor($numero_distribuidor)
+    {
+        $this->numero = $numero_distribuidor;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getTelefonesDistribuidor()
+    {
+        return $this->telefones;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function setTeledonesDistribuidor($telefones_distribuidor)
+    {
+        $this->telefones = $telefones_distribuidor;
+    }
 }
